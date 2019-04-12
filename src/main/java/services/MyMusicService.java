a625b02a032d868fac0999aefa26643b50e3ea30
bartokid <96bartolipau@gmail.com>
2019-04-12 19:22:49 +0200

    parte 1

--- src/main/java/services/MyMusicService.java
+++ src/main/java/services/MyMusicService.java
@@ -1,4 +1,19 @@
 package services;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 public class MyMusicService {
+
+
+
 }
--- src/test/java/PlaylistTest.java
+++ src/test/java/PlaylistTest.java
@@ -1,2 +1,28 @@
+import Manager.MyMusic;
+import Manager.MyMusicImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlaylistTest {
+    private MyMusic myMusic;
+
+    @Before
+    public void setUp() {
+
+        this.myMusic = MyMusicImpl.getInstance();
+
+    }
+
+    @After
+    public void tearDown() {
+        this.myMusic.clear();
+    }
+    @Test
+    public void playListTest(){
+
+    }
 }
