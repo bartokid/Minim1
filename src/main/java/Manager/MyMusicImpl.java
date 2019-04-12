57cc0b0675cd5ae7bc8779ec1d48ca1779d27d39
bartokid <96bartolipau@gmail.com>
2019-04-12 19:13:19 +0200

    log4 comments

--- src/main/java/Manager/MyMusicImpl.java
+++ src/main/java/Manager/MyMusicImpl.java
@@ -10,6 +10,7 @@ import java.util.LinkedList;
 
 public class MyMusicImpl implements MyMusic{
     //VARIABLES
+    final static Logger log = Logger.getLogger(Usuari.class);
     private static volatile MyMusicImpl instance = null;
     private HashMap<String, Usuari> usuaris;
     private LinkedList<Artista> artistes;
@@ -35,15 +36,18 @@ public class MyMusicImpl implements MyMusic{
     @Override
     public void addArtista( String name, String surname) {
         artistes.add(new Artista(name,surname));
+        log.trace("artista "+ name +" "+ surname +" afegit correctament");
     }
 
     @Override
     public void addUser( String usrId, Usuari usuari) {
         usuaris.put(usrId,usuari);
+        log.trace("usuari " +usuari.getName()+" afegit correctament");
     }
 
     @Override
     public List<Artista> getArtistes() {
+        log.info(artistes.toString());
         return this.artistes;
     }
 
@@ -53,6 +57,7 @@ public class MyMusicImpl implements MyMusic{
         Usuari usr = usuaris.get(usrId);
         usr.addPlaylist(playList);
         usuaris.put(usrId,usr);
+        log.trace("la playlist"+ titol+ "de l'usuari" +usuaris.get(usrId).getName()+ "ha estat afegida correctament");
     }
 
     @Override
@@ -60,10 +65,14 @@ public class MyMusicImpl implements MyMusic{
         Usuari usr = usuaris.get(usrId);
         usr.addPlaylistTittle(usr.getPlaylist(playlisId),tittle);
         usuaris.put(usrId,usr);
+        log.trace("titol de la playlist: " + usr.getPlaylist(playlisId).getName());
     }
 
     @Override
     public List<Playlist> getUserPlaylist(String usrId) {
+        log.trace("llista de playlist de l'usuari:" + usuaris.get(usrId).getName() +"\n"
+                + usuaris.get(usrId).getPlaylists().toString());
+
         return usuaris.get(usrId).getPlaylists();
     }
 
@@ -71,5 +80,6 @@ public class MyMusicImpl implements MyMusic{
     public void clear() {
         usuaris= new HashMap<>();
         artistes= new LinkedList<>();
+        log.trace("usuaris: "+ usuaris.toString() + "\nartistes" + artistes.toString());
     }
 }
